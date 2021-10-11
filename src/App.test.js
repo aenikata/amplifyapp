1fad0a989a57193e51a1de01d773489f8a250c7f
David Burton <github@grid5.co.uk>
2021-10-11 14:04:05 +0100

    V2

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('shows hello from v2', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Hello from V2/i);
+  expect(textElement).toBeInTheDocument();
 });
